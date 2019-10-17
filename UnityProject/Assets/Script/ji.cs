665af9e38964c30d597caa5db574a59ca4bca22b
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2019-10-17 12:06:39 +0800

    B1040434

---
+++ UnityProject/Assets/Script/ji.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ji : MonoBehaviour {
+    Animator myAnimator;
+	// Use this for initialization
+	void Start ()
+    {
+        myAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            myAnimator.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            myAnimator.SetBool("跑步開關", true);
+        }
+        else
+        {
+            myAnimator.SetBool("跑步開關",false);
+
+        }
+
+    }
+}
